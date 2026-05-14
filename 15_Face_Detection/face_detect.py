6a3af2e9f56231c7f6512413212e4726db9987e7
20danaai <270343442+20danaai@users.noreply.github.com>
2026-05-15 00:32:45 +0300

    Update face_detect.py

--- 15_Face_Detection/face_detect.py
+++ 15_Face_Detection/face_detect.py
@@ -1,6 +1,6 @@
 import cv2 as cv
 img=cv.imread('ai/Resources/Photos/group 1.jpg')
-cv.imshow('Group of 5 people',img)
+cv.imshow('Group ',img)
 gray=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 haar_cascade=cv.CascadeClassifier(r'C:\Users\PT\Desktop\hello\ai\Section #3 - Faces\haar_face.xml') # This line contains the AI file (trained algorithm) that recognizes the shape of a human face
 faces_rect=haar_cascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=1) # 1.1: This means we reduce the img size by 10% each time.
