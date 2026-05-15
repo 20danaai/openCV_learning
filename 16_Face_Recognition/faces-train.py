5f123a277c666fd33a73ed98dada810d2b371e2d
20danaai <270343442+20danaai@users.noreply.github.com>
2026-05-16 01:20:59 +0300

    Create faces-train.py

---
+++ 16_Face_Recognition/faces-train.py
@@ -0,0 +1,10 @@
+import os
+import numpy as np
+import cv2 as cv
+people=['Cristiano_Ronaldo','Lionel_Messi','Rodrigo_De_Paul']
+DIR=r'C:\Users\PT\Desktop\faces_train'
+features=[] # Facial features
+labels=[] # People numbers
+def creat_train():
+    pass
+print('Training setup initialized ..... ready for tomorrow!.')
